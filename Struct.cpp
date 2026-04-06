79da513a73d9739d8720483f977d2d8fc1e911b6
ZaynTamam <tamam.zain78@gmail.com>
2026-04-06 11:00:27 +0700

    Add main function to handle user input for mahasiswa struct

--- Struct.cpp
+++ Struct.cpp
@@ -7,3 +7,21 @@ struct mahasiswa
     string alamat;
     int umur;
 };
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa : " << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
